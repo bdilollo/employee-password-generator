a1c0d6e586cce5082c3e1f5993878f860e6a049e
bdilollo <113383715+bdilollo@users.noreply.github.com>
2022-09-29 01:03:38 -0500

    pseudocode for generating password + create character arrays

--- assets/script.js
+++ assets/script.js
@@ -9,11 +9,13 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+// series of prompts for password criteria
+
 var containLowercase;
 var containUppercase;
 var containNumbers;
 var containSpecial;
-var passwordLength;
 
 function criteritaPrompts() {
   alert("Select at least one criteria for your password.");
@@ -21,34 +23,63 @@ function criteritaPrompts() {
   containUppercase = confirm("Uppercase letters?");
   containNumbers = confirm("Numbers?");
   containSpecial = confirm("Special characters?");
-  passwordLength = prompt("How long would you like your password to be?");
   checkValidInput();
 }
+
+// validate input and at least one character type selected
 
 function checkValidInput() {
   if((containLowercase == false) && (containUppercase == false) && (containNumbers == false) && (containSpecial == false)) {
     alert("Invalid input. Please select at least one criteria to include in your password.");
     criteritaPrompts();
+  } else {
+    lengthPrompt();
   }
 }
-criteritaPrompts();
-checkValidInput()
+
+// prompt to choose length of password (between 8 and 128 characters)
+  var passwordLength;
+
+function lengthPrompt() {
+  passwordLength = prompt("How long would you like your password to be?");
+  checkValidLength();
+}
+
+// validate input--appropriate length
+function checkValidLength() {
+  if(passwordLength < 8 || passwordLength > 128) {
+    alert("Invalid length. Please enter a number between 8 and 128.");
+    lengthPrompt();
+  }
+}
+// criteritaPrompts();
+
+var lowercaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var uppercaseArray = lowercaseArray.map(element => {
+  return element.toUpperCase();
+});
+var numberArray = [0,1,2,3,4,5,6,7,8,9];
+var specialArray = ["!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"]
 
 function generatePassword() {
-  
+  // to generate password:
+  // create arrays to draw from for uppercase, lowercase, numbers, and special characters
+  // only using criteria where value=true
+  // concat arrays where value=true into one larger array
+  // Math.floor(Math.random() * length of larger array) to generate random number, use number as index to select a position from array
+  // add selected character to new password array
+  // repeat until length of password array matches passwordLength variable
 }
 
 // Add event listener to generate button
 // this calls the writePassword function defined above
 generateBtn.addEventListener("click", writePassword);
 
-// series of prompts for password criteria
 
-// prompt to select criteria for password
 
-// prompt to choose length of password (between 8 and 128 characters)
 
-// validate input and at least one character type selected
+
+
 
 // when all prompts answered, generate a password that matches criteria
 
